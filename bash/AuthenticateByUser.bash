b9de5aa3b183c187d75dc2dbfb51e50ef9620ec8
Tomi Haapaniemi <tomi.haapaniemi@apixmessaging.com>
2023-04-13 13:35:23 +0300

    add AuthenticateByUser.bash

---
+++ bash/AuthenticateByUser.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# ListInvoiceZIPs -method implemented as shell script
+# requirements: bash, sha256sum, curl
+
+[ "$#" -eq 3 ] || { echo "Usage: $0 <test/prod/ENDPOINT> <email> <password>" >&2; exit 1;}
+
+readonly ENVIRONMENT=$1;
+readonly EMAIL=$2;
+readonly PASSWORD=$3;
+
+ENDPOINT=$ENVIRONMENT;
+if [ "$ENVIRONMENT" = "prod" ]; then
+    ENDPOINT="https://api.apix.fi/authuser"
+elif [ "$ENVIRONMENT" = "test" ]; then
+    ENDPOINT="https://test-api.apix.fi/authuser"
+fi
+
+readonly TIMESTAMP=`date --utc +%Y%m%d%H%M%S`;
+
+#cut version
+readonly SHAPASS=$(echo -n "$PASSWORD"|sha256sum|cut -d ' ' -f 1);
+readonly DIGEST=$(echo -n "$EMAIL+$TIMESTAMP+$SHAPASS"|sha256sum|cut -d ' ' -f 1);
+
+readonly URL="${ENDPOINT}?uid=${EMAIL}&t=${TIMESTAMP}&d=SHA-256%3A${DIGEST}";
+
+curl -X GET $URL
+
